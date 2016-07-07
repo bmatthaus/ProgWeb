f0c9736bc4ddd75b94c7192e14296dcd1ff2f595
bmatthaus <bmmso@icomp.ufam.edu.br>
2016-07-07 18:01:22 -0400

    Add files via upload

---
+++ Trabalho_HTML03/Javascript2/d.js
@@ -0,0 +1,16 @@
+function desenhar(){
+
+	document.formi.ba1.height = document.formi.barra1.value;
+	document.formi.ba2.height = document.formi.barra2.value;
+	document.formi.ba3.height = document.formi.barra3.value;
+	document.formi.ba4.height = document.formi.barra4.value;
+	document.formi.ba5.height = document.formi.barra5.value;
+
+	document.formi.ba1.width = document.formi.barra6.value;
+	document.formi.ba2.width = document.formi.barra6.value;
+	document.formi.ba3.width = document.formi.barra6.value; 
+	document.formi.ba4.width = document.formi.barra6.value; 
+	document.formi.ba5.width = document.formi.barra6.value;
+
+
+}
